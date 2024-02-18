141c79e0407c7be20b2c5e1013eb45a468e59c5a
Forgetful-coder <aayushaggarwal243@gmail.com>
2024-02-18 14:31:50 -0500

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ from dataclasses import dataclass
 import os
 import sys
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataingestionConfig:
@@ -43,7 +46,11 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
             
     
 
